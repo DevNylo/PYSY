79dac1d394f567292caecc927503b0d49af47265
Nylo <contato_dsr@hotmail.com>
2022-07-25 12:22:03 -0300

    Update script-tarde.py

--- Turno - 1316/script-tarde.py
+++ Turno - 1316/script-tarde.py
@@ -5,12 +5,12 @@ import time
 
 pyautogui.moveTo(470, 95) # X
 pyautogui.click(button='left') # CLOSE CAD
-time.sleep(7)
+time.sleep(8)
 pyautogui.moveTo(722, 776) # MOVE TO SAVE
 pyautogui.click(button='left') # SAVE
 time.sleep(1)
 pyautogui.click(button='left')
-time.sleep(7)
+time.sleep(7.5)
 pyautogui.press('r')
 pyautogui.press('r')
 time.sleep(1)
