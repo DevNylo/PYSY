ed3b4281be878233b13b8cbe62af8b08c21ee149
Nylo <contato_dsr@hotmail.com>
2022-07-26 09:17:43 -0300

    UPDATE

--- Turno - 712/script-karla.py
+++ Turno - 712/script-karla.py
@@ -4,7 +4,7 @@ import time
 
 pyautogui.moveTo(470, 95) # X
 pyautogui.click(button='left') # CLOSE CAD
-time.sleep(8)
+time.sleep(12)
 pyautogui.moveTo(105, 601) # MOVE TO "PROFISSIONAL RESPONSÁVEL"
 time.sleep(1)
 pyautogui.click(button='left')
@@ -23,7 +23,7 @@ pyautogui.write('16159083') # PHYSIOTHERAPIST JOSE
 time.sleep(1)
 pyautogui.moveTo(722, 776) # MOVE TO SAVE
 pyautogui.click(button='left') # SAVE
-time.sleep(7)
+time.sleep(12)
 pyautogui.moveTo(927, 362) # MOVE TO "SETORES"
 pyautogui.click(button='left') # SELECT "SETOR"
 time.sleep(2)
