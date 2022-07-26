2c2e2179fb5e9b81501985183e09a4ee8bfead7f
Nylo <contato_dsr@hotmail.com>
2022-07-26 10:59:42 -0300

    Update script-jose.py

--- Turno - 712/script-jose.py
+++ Turno - 712/script-jose.py
@@ -4,7 +4,7 @@ import time
 
 pyautogui.moveTo(470, 95) # X
 pyautogui.click(button='left') # CLOSE CAD
-time.sleep(38)
+time.sleep(39)
 pyautogui.moveTo(105, 601) # MOVE TO "PROFISSIONAL RESPONSÁVEL"
 time.sleep(1)
 pyautogui.click(button='left')
